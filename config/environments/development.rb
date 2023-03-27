e8ba79efee0b5f98ffbc44fa9e246771583d519d
pramag0314 <galois314159265358@i.softbank.jp>
2023-03-27 15:06:58 +0000

    Add hello

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  # Cloud9 への接続を許可する
+  config.hosts.clear
 end
