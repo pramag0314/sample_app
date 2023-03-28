95a18a6cc80082d39f87c08c33c599539ea72fe1
pramag0314 <galois314159265358@i.softbank.jp>
2023-03-28 15:18:44 +0000

    static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'foo/bar'
+  get 'foo/baz'
+  get 'static_pages/home'
+  get 'static_pages/help'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 root "application#hello"
   # Defines the root path route ("/")
